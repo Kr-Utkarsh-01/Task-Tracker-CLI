8df86f8be8bf241439ede2922adebf3e9a10d469
Kumar Utkarsh <kumaru@iitbhilai.ac.in>
2025-10-19 04:39:09 +0530

    Added function to update tasks

--- taskAppScript.py
+++ taskAppScript.py
@@ -1,5 +1,7 @@
+from operator import ne
 import sys
 import json
+from tkinter import NO
 def addTask(data, task):
     count=data["count"]
     newTask={
@@ -13,7 +15,11 @@ def addTask(data, task):
     f.close()
     print(f"Added task: '{task}' | task id: {count+1}")
 
-    
+def updateTask(data, id, newTask):
+    data[id]=newTask
+    f=open("tasks.json", "w")
+    json.dump(data, f, indent=4)
+    print(f"Task with id={id} has been updated to: '{newTask}'")
 
 #main()
 f=open("tasks.json", "r")
