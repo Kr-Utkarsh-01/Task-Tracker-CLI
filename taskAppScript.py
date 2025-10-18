897d460d5a7edacea4c17ef77251dace960e4f9e
Kumar Utkarsh <kumaru@iitbhilai.ac.in>
2025-10-19 03:13:24 +0530

    Added function to add new tasks

--- taskAppScript.py
+++ taskAppScript.py
@@ -1,3 +1,16 @@
 import sys
 import json
 def addTask(task):
+    f=open("tasks.json", "r")
+    data=json.load(f)
+    count=data["count"]
+    newTask={
+                "task": task,
+                "status": "To-do"
+            }
+    data[count+1]=newTask
+    data["count"]+=1
+    f.close()
+    f=open("tasks.json", "w")
+    json.dump(data, f, indent=4)
+    f.close()
